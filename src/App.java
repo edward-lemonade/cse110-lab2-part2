0e85cb4e6ea54ad4902195119d74ba083134c796
Edward <bepbananas@gmail.com>
2026-01-12 14:32:51 -0800

    calls display now

--- src/App.java
+++ src/App.java
@@ -1,8 +1,6 @@
-package edu.ucsd.cse110.lab3;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        display("Hello world");
     }
 
     public static void display(String message) {
